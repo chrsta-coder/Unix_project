c48532595eaac304c05489122234b704b40d9c0f
chrsta-coder <christbilla.chrsta@gmail.com>
2026-08-27 16:41:23 +0200

    added the current directory

---
+++ guessinggame.sh
@@ -0,0 +1,32 @@
+# guess the number of files
+
+# definition of the function
+
+function guess_number {
+
+	if [[ $nub -eq $(ls | wc -l) ]]
+	then
+		echo "Congratulation, you find the correct number"
+	elif [[ $nub -lt $(ls | wc -l) ]]
+	then
+		echo "your number is smaller than the answer"
+		read nub
+	else
+		echo "your number is bigger than the answer"
+		read nub
+	fi
+
+}
+
+#program begin
+
+echo "Guess how many files are in the current directory ?"
+read nub
+
+while [[ $nub != $(ls | wc -l) ]]
+do
+
+	guess_number
+done
+
+echo "Congratulation, you find the correct number"
